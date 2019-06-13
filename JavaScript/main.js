e8843e8eaf91915cebdbab2be7b5ebfc75d1238e
Faith Wallace <fwallacephd@gmail.com>
2019-06-13 11:05:43 -0400

    add and delete buttons

---
+++ JavaScript/main.js
@@ -0,0 +1,39 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("companions").addEventListener("click",  function() {
+    document.getElementById("companions").style.display = "none";
+    document.getElementById("aliens").style.display = "none";
+    document.getElementById("doctor").style.display = "none";
+
+    document.getElementById("easy").style.display = "unset";
+    document.getElementById("medium").style.display = "unset";
+    document.getElementById("hard").style.display = "unset";
+  })
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("aliens").addEventListener("click",  function() {
+    document.getElementById("companions").style.display = "none";
+    document.getElementById("aliens").style.display = "none";
+    document.getElementById("doctor").style.display = "none";
+
+    document.getElementById("easy").style.display = "unset";
+    document.getElementById("medium").style.display = "unset";
+    document.getElementById("hard").style.display = "unset";
+  })
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("doctor").addEventListener("click",  function() {
+    document.getElementById("companions").style.display = "none";
+    document.getElementById("aliens").style.display = "none";
+    document.getElementById("doctor").style.display = "none";
+
+    document.getElementById("easy").style.display = "unset";
+    document.getElementById("medium").style.display = "unset";
+    document.getElementById("hard").style.display = "unset";
+  })
+});
+
+//Companions - Easy Who is Bad Wolf, What is the na
+
