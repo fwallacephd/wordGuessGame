b2bf3ee22614120f3c1f818d380e3d3c7d08ba38
Faith Wallace <fwallacephd@gmail.com>
2019-06-14 03:41:29 -0400

    added questions

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,18 +1,18 @@
+//How do I hide the questions so that they only show up when you are ready to guess.
+//How do I write a conditional that uses both the category and  difficulty to display the right questonn. 
+
 document.addEventListener("DOMContentLoaded", function() {
   document.getElementById("companions").addEventListener("click",  function() {
     document.getElementById("companions").style.display = "none";
     document.getElementById("aliens").style.display = "none";
     document.getElementById("doctor").style.display = "none";
-    
-    document.getElementsByClassName("easy").style.display = 'none';
 
     document.getElementById("easy").style.display = "unset";
     document.getElementById("medium").style.display = "unset";
     document.getElementById("hard").style.display = "unset";
-  })
-});
 
-document.addEventListener("DOMContentLoaded", function() {
+  })
+
   document.getElementById("aliens").addEventListener("click",  function() {
     document.getElementById("companions").style.display = "none";
     document.getElementById("aliens").style.display = "none";
@@ -22,9 +22,7 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("medium").style.display = "unset";
     document.getElementById("hard").style.display = "unset";
   })
-});
 
-document.addEventListener("DOMContentLoaded", function() {
   document.getElementById("doctor").addEventListener("click",  function() {
     document.getElementById("companions").style.display = "none";
     document.getElementById("aliens").style.display = "none";
@@ -34,25 +32,35 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("medium").style.display = "unset";
     document.getElementById("hard").style.display = "unset";
   })
-});
 
-document.addEventListener("DOMContentLoaded", function() {
   document.getElementById("easy").addEventListener("click",  function() {
-    document.getElementById("easy>").style.display = "none";
+    document.getElementById("easy").style.display = "none";
     document.getElementById("medium").style.display = "none";
     document.getElementById("hard").style.display = "none";
 
-    document.getElementById("easy").style.display = "unset";
-    document.getElementById("medium").style.display = "unset";
-    document.getElementById("hard").style.display = "unset";
+    document.querySelector(".easyCom").style.display = "unset";
+  })
 
-    document.getElementById("companions").style.display = "none";
-    document.getElementById("aliens").style.display = "none";
-    document.getElementById("doctor").style.display = "none";
+  document.getElementById("medium").addEventListener("click",  function() {
+    document.getElementById("easy").style.display = "none";
+    document.getElementById("medium").style.display = "none";
+    document.getElementById("hard").style.display = "none";
+
+    document.querySelector(".mediumCom").style.display = "unset";
+  })
+
+  document.getElementById("hard").addEventListener("click",  function() {
+    document.getElementById("easy").style.display = "none";
+    document.getElementById("medium").style.display = "none";
+    document.getElementById("hard").style.display = "none";
+
+    document.querySelector(".hardCom").style.display = "unset";
   })
 });
 
-companionsEasy
+
+
+
     
 
 
