d2383917e4ea12b10c29d07704766659dd606529
Faith Wallace <fwallacephd@gmail.com>
2019-06-14 05:20:43 -0400

    added all of the category questions

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,8 +1,6 @@
-//How do I hide the questions so that they only show up when you are ready to guess.
-//How do I write a conditional that uses both the category and  difficulty to display the right questonn. 
-
-document.addEventListener("DOMContentLoaded", function() {
-  document.getElementById("companions").addEventListener("click",  function() {
+document.addEventListener("DOMContentLoaded", function () {
+  //companions
+  document.getElementById("companions").addEventListener("click", function () {
     document.getElementById("companions").style.display = "none";
     document.getElementById("aliens").style.display = "none";
     document.getElementById("doctor").style.display = "none";
@@ -11,9 +9,91 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("medium").style.display = "unset";
     document.getElementById("hard").style.display = "unset";
 
+    document.getElementById("easy").addEventListener("click", function () {
+      document.getElementById("easy").style.display = "none";
+      document.getElementById("medium").style.display = "none";
+      document.getElementById("hard").style.display = "none";
+
+      const element = document.getElementById("easyCheck");
+      if (element.classList.contains('easyCom')) {
+        document.querySelector(".easyCom").style.display = "unset";
+      }
+    })
+
+    document.getElementById("medium").addEventListener("click", function () {
+      document.getElementById("easy").style.display = "none";
+      document.getElementById("medium").style.display = "none";
+      document.getElementById("hard").style.display = "none";
+
+      const element = document.getElementById("mediumCheck");
+      if (element.classList.contains('mediumCom')) {
+        document.querySelector(".mediumCom").style.display = "unset";
+      }
+    })
+
+    document.getElementById("hard").addEventListener("click", function () {
+      document.getElementById("easy").style.display = "none";
+      document.getElementById("medium").style.display = "none";
+      document.getElementById("hard").style.display = "none";
+
+      const element = document.getElementById("hardCheck");
+      if (element.classList.contains('hardCom')) {
+        document.querySelector(".hardCom").style.display = "unset";
+      }
+    })
+
   })
 
-  document.getElementById("aliens").addEventListener("click",  function() {
+  //companion end
+
+  //alien
+  document.getElementById("aliens").addEventListener("click", function () {
+    document.getElementById("companions").style.display = "none";
+    document.getElementById("aliens").style.display = "none";
+    document.getElementById("doctor").style.display = "none";
+
+    document.getElementById("easy").style.display = "unset";
+    document.getElementById("medium").style.display = "unset";
+    document.getElementById("hard").style.display = "unset";
+
+    document.getElementById("easy").addEventListener("click", function () {
+      document.getElementById("easy").style.display = "none";
+      document.getElementById("medium").style.display = "none";
+      document.getElementById("hard").style.display = "none";
+
+      const element = document.getElementById("easyAlienCheck");
+      if (element.classList.contains('easyAlien')) {
+        document.querySelector(".easyAlien").style.display = "unset";
+      }
+    })
+
+    document.getElementById("medium").addEventListener("click", function () {
+      document.getElementById("easy").style.display = "none";
+      document.getElementById("medium").style.display = "none";
+      document.getElementById("hard").style.display = "none";
+
+      const element = document.getElementById("mediumAlienCheck");
+      if (element.classList.contains('mediumAlien')) {
+        document.querySelector(".mediumAlien").style.display = "unset";
+      }
+    })
+
+    document.getElementById("hard").addEventListener("click", function () {
+      document.getElementById("easy").style.display = "none";
+      document.getElementById("medium").style.display = "none";
+      document.getElementById("hard").style.display = "none";
+
+      const element = document.getElementById("hardAlienCheck");
+      if (element.classList.contains('hardAlien')) {
+        document.querySelector(".hardAlien").style.display = "unset";
+      }
+    })
+  })
+  //alien end
+
+
+//doctor
+  document.getElementById("doctor").addEventListener("click", function () {
     document.getElementById("companions").style.display = "none";
     document.getElementById("aliens").style.display = "none";
     document.getElementById("doctor").style.display = "none";
@@ -23,45 +103,39 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("hard").style.display = "unset";
   })
 
-  document.getElementById("doctor").addEventListener("click",  function() {
-    document.getElementById("companions").style.display = "none";
-    document.getElementById("aliens").style.display = "none";
-    document.getElementById("doctor").style.display = "none";
-
-    document.getElementById("easy").style.display = "unset";
-    document.getElementById("medium").style.display = "unset";
-    document.getElementById("hard").style.display = "unset";
-  })
-
-  document.getElementById("easy").addEventListener("click",  function() {
+  document.getElementById("easy").addEventListener("click", function () {
     document.getElementById("easy").style.display = "none";
     document.getElementById("medium").style.display = "none";
     document.getElementById("hard").style.display = "none";
 
-    document.querySelector(".easyCom").style.display = "unset";
+    const element = document.getElementById("easyDoctorCheck");
+    if (element.classList.contains('easyDoctor')) {
+      document.querySelector(".easyDoctor").style.display = "unset";
+    }
   })
 
-  document.getElementById("medium").addEventListener("click",  function() {
+  document.getElementById("medium").addEventListener("click", function () {
     document.getElementById("easy").style.display = "none";
     document.getElementById("medium").style.display = "none";
     document.getElementById("hard").style.display = "none";
 
-    document.querySelector(".mediumCom").style.display = "unset";
+    const element = document.getElementById("mediumDoctorCheck");
+    if (element.classList.contains('mediumDoctor')) {
+      document.querySelector(".mediumDoctor").style.display = "unset";
+    }
   })
 
-  document.getElementById("hard").addEventListener("click",  function() {
+  document.getElementById("hard").addEventListener("click", function () {
     document.getElementById("easy").style.display = "none";
     document.getElementById("medium").style.display = "none";
     document.getElementById("hard").style.display = "none";
 
-    document.querySelector(".hardCom").style.display = "unset";
+    const element = document.getElementById("hardDoctorCheck");
+    if (element.classList.contains('hardDoctor')) {
+      document.querySelector(".hardDoctor").style.display = "unset";
+    }
   })
+
 });
 
-
-
-
-    
-
-
-
+//doctor end
